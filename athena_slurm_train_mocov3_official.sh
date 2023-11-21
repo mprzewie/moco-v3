9739536b1308261264ffdd649fd87a83cd8f9bbb
Marcin Przewiezlikowski <m.przewie@gmail.com>
2023-11-21 10:14:45 +0100

    athena script

--- athena_slurm_train_mocov3_official.sh
+++ athena_slurm_train_mocov3_official.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=MoCoV3_official
-#SBATCH --gpus=8
+#SBATCH --gpus=6
 #SBATCH --mem-per-cpu=16G
 #SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
@@ -20,5 +20,5 @@ cd $HOME/uj/moco-v3
 
 # override imagenet100 with imagenet
 
-python main_moco.py -a vit_base --cassle --lr 1.5e-4 --weight-decay 0.1 --stop-grad-conv1 --moco-t 0.2 --moco-m-cos --moco-mlp-dim 4096 --moco-dim 256  --batch-size 1024  --warmup-epochs=40 --epochs 300 --dist-url 'tcp://localhost:8008' --multiprocessing-distributed --world-size 1 --rank 0 "$PLG_GROUPS_STORAGE/plgg_gmum_cc/datasets/ImageNet/ILSVRC/Data/CLS-LOC"
+python main_moco.py -a vit_base --cassle --lr 1.5e-4 --weight-decay 0.1 --stop-grad-conv1 --moco-t 0.2 --moco-m-cos --moco-mlp-dim 4096 --moco-dim 256  --batch-size 1024  --warmup-epochs=40 --epochs 300 --dist-url 'tcp://localhost:8008' --multiprocessing-distributed --world-size 1 --rank 0 "$PLG_GROUPS_STORAGE/plgg_gmum_cc/datasets/ImageNet/ILSVRC/Data/CLS-LOC" --save-dir mocov3_cassle_6gpu
 
